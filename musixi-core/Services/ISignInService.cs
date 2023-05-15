873f9275ea6c9e4ae42735828b629c1f4bd18d97
yusufhaydarlok <69205369+yusufhaydarlok@users.noreply.github.com>
2023-05-15 17:39:46 +0300

    sadasd

---
+++ musixi-core/Services/ISignInService.cs
@@ -0,0 +1,14 @@
+﻿using musixi_core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace musixi_core.Services
+{
+    public interface ISignInService
+    {
+        Task<UserDto> Login(string username, string password);
+    }
+}
